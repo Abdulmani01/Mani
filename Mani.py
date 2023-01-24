c9a6970d9ec99208628d686a6323f544c7a3ba87
Abdulmani01 <123299610+Abdulmani01@users.noreply.github.com>
2023-01-24 20:50:52 +0500

    Create Mani.py

---
+++ Mani.py
@@ -0,0 +1,13 @@
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('xdg-open https://facebook.com/groups/3465474777107830/')
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import M1
+elif bit == '32bit':
+    import M1
+ 
